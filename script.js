e5023a67a1a9cdbd5b1aa62a0259488519fb29b6
John Barker <jdabarker@gmail.com>
2021-03-27 20:59:35 +0800

    added questions. added question display logic

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 const jStartButton = $('#start');
 const jLeaderboardButton = $('#leaderboard');
-const jQuestion = $('#question');
+const jQuestionBox = $('#question');
 const jTime = $('#time');
 
 let questions = [];
+let currentQuestion = 0;
 let timer = 0;
 
 $.getJSON('questions.json', function(json) {
+    if (!json) {
+        alert('could not load questions.json');
+        return;
+    }
     questions = json['questions'];
-    jStartButton.on('click', startGame);
     readyGame();
+    jStartButton.on('click', startGame);
 })
 
 function readyGame() {
     timer = 0;
     jTime.text(timer);
-    const question = getRandomQuestion();
-    console.log(question);
-    const questionText = question[0];
-    console.log(questionText);
-    const jNewQuestion = $('<h2>').text(questionText);
-    jQuestion.append(jNewQuestion);
+}
+
+function displayNewQuestion() {
+    jQuestionBox.empty();
+    const questionObject = getNextQuestion();
+    const jQuestion = $('<h2>').text(questionObject.questionText);
+    jQuestionBox.append(jQuestion);
+    const numOptions = 4;
+    for (let i = 1; i < numOptions + 1; i++) {
+        const jAnswer = $('<button>').text(i + ') ' + questionObject['option' + i]);
+        jAnswer.css('display', 'block');
+        jAnswer.addClass('btn-lg btn-primary m-3 text-left')
+        jQuestionBox.append(jAnswer);
+    }
 }
 
 function startGame() {
-    
+    jStartButton.attr('disabled', true);
+    jStartButton.animate({ opacity: 0 });
+    displayNewQuestion();
 }
 
-function getRandomQuestion() {
-    return questions[Math.floor(Math.random() * questions.length)];
+function getNextQuestion() {
+    const totalQuestions = 5;
+    if (currentQuestion == totalQuestions) {
+        endGame();
+        return;
+    }
+    const nextQuestion = questions[currentQuestion];
+    currentQuestion++;
+    return nextQuestion;
+}
+
+function endGame() {
+
 }
